b26a33f4cee7c0dd444b56c301d53ca78d0dc3f8
duboy <futuredby@gmail.com>
2017-10-27 16:30:26 +0800

    7.3.2

--- ch07/ex7_27.h
+++ ch07/ex7_27.h
@@ -6,10 +6,37 @@ public:
 	using pos = std::string::size_type;
 	
 	Screen () = default;
-	Screen (pos &w, pos &h) : cursor(w), width(h), contents(h*w, ' ') {}
-	Screen (pos &w, pos &h, std::string &s) : cursor(w), width(h), contents(s) {}
+	Screen (pos w, pos h) : width(w), height(h), contents(h*w, ' ') {}
+	Screen (pos w, pos h, char s) : width(w), height(h), contents(w*h, s) {}
+	Screen &move (pos r, pos c);
+	Screen &set (char c);
+	Screen &display (std::ostream &os)
+	{
+		do_display (os);
+		return *this;
+	}
+	const Screen &display (std::ostream &os) const
+	{
+		do_display (os);
+		return *this;
+	}
 private:
 	pos cursor = 0;
 	pos height = 0, width = 0;
 	std::string contents;
+	
+	void do_display (std::ostream &os) 
+	{
+		os << contents;
+	}
 };
+inline Screen &Screen::move (pos r, pos c) const
+{
+	cursor = r * width + c;
+	return *this;
+}
+inline Screen &Screen::set (char c)
+{
+	contents[cursor] = c;
+	return *this;
+}
--- ch07/test_2.cpp
+++ ch07/test_2.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
-#include "ex7_24.h"
+#include "ex7_27.h"
+using namespace std;
 
 int main ()
 {
-	Screen screen;
-	
+	Screen myScreen (5, 5, 'X');
+	myScreen.move (4, 0).set ('#').display (cout);
+	cout << "\n";
+	myScreen.display (cout);
+	cout << "\n";	
 }
