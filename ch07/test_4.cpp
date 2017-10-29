493aa68ca5ec5e6c4a69bf15a7c2fccd6b4d0bbf
duboy <futuredby@gmail.com>
2017-10-29 15:13:48 +0800

    7.4

---
+++ ch07/test_4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "ex7_33.h"
+
+int main ()
+{
+	Window_mgr winMgr;
+	Screen s(2, 3, 'X');
+	winMgr.screens.push_back (s);
+	winMgr.screens[0].display (std::cout);
+	std::cout << '\n';
+	winMgr.clear(0);
+	winMgr.screens[0].display (std::cout);
+	std::cout << '\n';
+}
